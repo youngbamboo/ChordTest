33073654364a4740faf5458634a226e4a7cff25a
Zhu Yang <youngbamboo@gmail.com>
2013-10-12 14:23:48 -0600

    Add chord service

---
+++ ChordService.h
@@ -0,0 +1,25 @@
+#ifndef _CHORDSERVICE_H
+#define _CHORDSERVICE_H
+
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class ChordService {
+	private:
+		string myHostName;
+		string myIP;
+		int myID;
+			
+			
+};
+
+#endif
+
