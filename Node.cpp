aad1f020fba41b64fbd9491b70575fb254bb6555
youngbamboo <youngbamboo@gmail.com>
2013-10-16 21:38:57 -0600

    s

--- Node.cpp
+++ Node.cpp
@@ -39,7 +39,7 @@ Node::Node()
 	myID = buildHashID(myIP);
 	cout<<"My Hash ID is: "<<myID<<endl;
 
-	mkdir(myDirectory.c_str());
+	mkdir(myDirectory.c_str(),666);
 	//Remove all files.
 	system("exec rm -r /tmp/zyang");
 
